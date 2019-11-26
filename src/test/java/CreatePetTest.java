21e873e3b74a89593207ada95df79ae690d414fd
Yurii Slynko <ummbriaco@gmail.com>
2019-11-26 13:14:24 +0200

    Lesson 7

--- src/test/java/CreatePetTest.java
+++ src/test/java/CreatePetTest.java
@@ -1,92 +1,72 @@
-import io.restassured.RestAssured;
-import io.restassured.http.ContentType;
 import io.restassured.response.ValidatableResponse;
-import io.restassured.specification.RequestSpecification;
 import org.junit.Before;
-import org.junit.FixMethodOrder;
 import org.junit.Test;
-import org.junit.runners.MethodSorters;
 
 import static org.hamcrest.CoreMatchers.*;
 import static org.hamcrest.core.Is.is;
 
-@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class CreatePetTest {
 
-    static long petId;
+    private PetEndpoint petEndpoint = new PetEndpoint();
 
-    public RequestSpecification given() {
-        return RestAssured
-                .given()
-                .baseUri("https://petstore.swagger.io/v2")
-                .log().all()
-                .contentType(ContentType.JSON);
-    }
+    private static long petId;
+    private String body = "{\n" +
+            "  \"id\": 0,\n" +
+            "  \"category\": {\n" +
+            "    \"id\": 0,\n" +
+            "    \"name\": \"string\"\n" +
+            "  },\n" +
+            "  \"name\": \"varan\",\n" +
+            "  \"photoUrls\": [\n" +
+            "    \"string\"\n" +
+            "  ],\n" +
+            "  \"tags\": [\n" +
+            "    {\n" +
+            "      \"id\": 0,\n" +
+            "      \"name\": \"string\"\n" +
+            "    }\n" +
+            "  ],\n" +
+            "  \"status\": \"available\"\n" +
+            "}";
 
     @Before
-    public void CreatePet() {
+    public void beforeMethod(){
 
-        String body = "{\n" +
-                "  \"id\": 0,\n" +
-                "  \"category\": {\n" +
-                "    \"id\": 0,\n" +
-                "    \"name\": \"string\"\n" +
-                "  },\n" +
-                "  \"name\": \"varan\",\n" +
-                "  \"photoUrls\": [\n" +
-                "    \"string\"\n" +
-                "  ],\n" +
-                "  \"tags\": [\n" +
-                "    {\n" +
-                "      \"id\": 0,\n" +
-                "      \"name\": \"string\"\n" +
-                "    }\n" +
-                "  ],\n" +
-                "  \"status\": \"available\"\n" +
-                "}";
-
-        ValidatableResponse response = given()
-                .body(body)
-                .post(PetEndpoint.CREATE_PET)
-                .then()
-                .statusCode(anyOf(is(200), is(201)))
-                .body("category.name", is(not("")))
-                .log().all();
+        ValidatableResponse response = petEndpoint
+                .createPet(body)
+                .statusCode(is(200))
+                .body("category.name", is(not("")));
         petId = response.extract().body().path("id");
     }
 
     @Test
-    public void test1CreatePet(){
-        given()
-                .response()
-                .statusCode(anyOf(is(200), is(201)))
-                .body("name", is("varan"))
-                .log().all();
+    public void createPetTest () {
+
+        ValidatableResponse response =
+                petEndpoint
+                        .createPet(body)
+                        .statusCode(is(200))
+                        .body("category.name", is(not("")));
     }
 
     @Test
-    public void test2GetPetById(){
-        given()
-                .get(PetEndpoint.GET_PET, petId)
-                .then()
-                .statusCode(anyOf(is(200), is(201)))
-                .body("category.name", is(not("")))
-                .log().all();
+    public void getPetById(){
+        petEndpoint
+                .getPet(petId)
+                .statusCode(is(200))
+                .body("category.name", is(not("")));
     }
 
     @Test
-    public void test3DeletePetById(){
-        given()
-                .delete(PetEndpoint.DELETE_PET, petId)
-                .then()
-                .statusCode(anyOf(is(200), is(201)))
-                .log().all();
-        given()
-                .get(PetEndpoint.GET_PET, petId)
-                .then()
+    public void deletePetById(){
+        petEndpoint
+                .deletePet(petId)
+                .statusCode(is(200));
+
+        petEndpoint
+                .getPet(petId)
                 .statusCode(is(404))
-                .body("message", is("Pet not found"))
-                .log().all();
+                .body("message", is("Pet not found"));
     }
 
 }
--- src/test/java/PetEndpoint.java
+++ src/test/java/PetEndpoint.java
@@ -1,5 +1,44 @@
+import io.restassured.RestAssured;
+import io.restassured.filter.log.LogDetail;
+import io.restassured.filter.log.RequestLoggingFilter;
+import io.restassured.filter.log.ResponseLoggingFilter;
+import io.restassured.http.ContentType;
+import io.restassured.response.ValidatableResponse;
+import io.restassured.specification.RequestSpecification;
+
 public class PetEndpoint {
     public final static String CREATE_PET = "/pet";
-    public  final static String GET_PET = "/pet/{petId}";
-    public  final static String DELETE_PET = "/pet/{petId}";
+    public final static String GET_PET = "/pet/{petId}";
+    public final static String DELETE_PET = "/pet/{petId}";
+
+    static {
+        RestAssured.filters(new ResponseLoggingFilter(LogDetail.ALL));
+        RestAssured.filters(new RequestLoggingFilter(LogDetail.ALL));
+    }
+
+    public RequestSpecification given() {
+        return RestAssured
+                .given()
+                .baseUri("https://petstore.swagger.io/v2")
+                .contentType(ContentType.JSON);
+    }
+
+    public ValidatableResponse createPet (String body) {
+        return given()
+                .body(body)
+                .post(CREATE_PET)
+                .then();
+    }
+
+    public ValidatableResponse getPet (long petId) {
+        return given()
+                .get(GET_PET, petId)
+                .then();
+    }
+
+    public ValidatableResponse deletePet (long petId) {
+        return given()
+                .delete(DELETE_PET, petId)
+                .then();
+    }
 }
